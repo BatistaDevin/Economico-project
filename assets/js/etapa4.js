04956b19d8a43f6dbeeb76af5d2ae77148e7db8e
Lucas Batista <140105766+BatistaDevin@users.noreply.github.com>
2025-08-04 02:42:44 -0300

    v5

---
+++ assets/js/etapa4.js
@@ -0,0 +1,192 @@
+const slides = [
+  {
+    imagem: "../images/bifes/acem pedaco.png",
+    descricao: "Acém - Corte dianteiro, ótimo para cozidos ou moído."
+  },
+  {
+    imagem: "../images/bifes/acem cubo.png",
+    descricao: "Acém em Cubos - Usado em ensopados e cozidos de longo preparo."
+  },
+  {
+    imagem: "../images/bifes/Acem moido.png",
+    descricao: "Acém Moído - Saboroso e versátil, ideal para hambúrgueres e molhos."
+  },
+  {
+    imagem: "../images/bifes/paleta pedaco.png",
+    descricao: "Paleta - Corte dianteiro, muito utilizado em cozidos e carnes de panela."
+  },
+  {
+    imagem: "../images/bifes/paleta moida.png",
+    descricao: "Paleta Moída - Boa para recheios e pratos do dia a dia."
+  },
+  {
+    imagem: "../images/bifes/coxao duro pedaco.png",
+    descricao: "Coxão Duro - Ideal para cozidos e carne de panela."
+  },
+  {
+    imagem: "../images/bifes/coxao duro moido.png",
+    descricao: "Coxão Duro Moído - Ótimo para hambúrgueres e recheios."
+  },
+  {
+    imagem: "../images/bifes/bife duro.png",
+    descricao: "Coxão Duro em Bife - Precisa de preparo adequado para maciez."
+  },
+  {
+    imagem: "../images/bifes/bife a role coxao duro.png",
+    descricao: "Bife à Rolê - Recheado e enrolado, ideal com molho."
+  },
+  {
+    imagem: "../images/bifes/largato pedaco.png",
+    descricao: "Lagarto - Corte magro, excelente para rosbife ou assado."
+  },
+  {
+    imagem: "../images/bifes/bife lagarto.png",
+    descricao: "Lagarto em Bifes - Pode ser grelhado ou usado em receitas com molho."
+  },
+  {
+    imagem: "../images/bifes/contra pedaco.png",
+    descricao: "Contra Filé - Macio e suculento, ótimo para grelha."
+  },
+  {
+    imagem: "../images/bifes/bife contra.png",
+    descricao: "Bife de Contra Filé - Corte tradicional para grelhar ou fritar."
+  },
+  {
+    imagem: "../images/bifes/contra especial.png",
+    descricao: "Contra Filé Especial - Com marmoreio, excelente para churrasco."
+  },
+  {
+    imagem: "../images/bifes/bife ancho.png",
+    descricao: "Bife Ancho - Parte nobre do contra filé, muito suculento."
+  },
+  {
+    imagem: "../images/bifes/alcatra pedaco.png",
+    descricao: "Alcatra - Corte macio e versátil, bom para bifes e grelhados."
+  },
+  {
+    imagem: "../images/bifes/bife alcatra.png",
+    descricao: "Bife de Alcatra - Saboroso e ideal para fritar ou grelhar."
+  },
+  {
+    imagem: "../images/bifes/alcatra moida.png",
+    descricao: "Alcatra Moída - Excelente para receitas mais magras e recheios."
+  },
+  {
+    imagem: "../images/bifes/strogonoff alcatra.png",
+    descricao: "Alcatra em Tiras - Ideal para strogonoff ou picadinho."
+  },
+  {
+    imagem: "../images/bifes/maminha.png",
+    descricao: "Maminha - Extremidade da alcatra, muito usada em churrascos."
+  },
+  {
+    imagem: "../images/bifes/bifes maminha.png",
+    descricao: "Bife de Maminha - Saborosa e suculenta, ótima para grelhar."
+  },
+  {
+    imagem: "../images/bifes/coxao mole pedaco.png",
+    descricao: "Coxão Mole - Macio, ideal para bifes e assados rápidos."
+  },
+  {
+    imagem: "../images/bifes/cubos coxao mole.png",
+    descricao: "Coxão Mole em Cubos - Usado em ensopados e refogados."
+  },
+  {
+    imagem: "../images/bifes/mole moido.png",
+    descricao: "Coxão Mole Moído - Opção magra para bolonhesas e recheios."
+  },
+  {
+    imagem: "../images/bifes/bife coxao mole.png",
+    descricao: "Bife de Coxão Mole - Rápido preparo e sabor delicado."
+  },
+  {
+    imagem: "../images/bifes/bife coxao mole especial.png",
+    descricao: "Bife de Coxão Mole Especial - Seleção premium para grelhar."
+  },
+  {
+    imagem: "../images/bifes/strogonoff coxao mole.png",
+    descricao: "Coxão Mole em Tiras - Ideal para strogonoff ou picadinhos."
+  },
+  {
+    imagem: "../images/bifes/patinho pedaco.png",
+    descricao: "Patinho - Corte magro e versátil, ótimo para moer ou bifes."
+  },
+  {
+    imagem: "../images/bifes/patinho moido.png",
+    descricao: "Patinho Moído - Ótima opção para hambúrgueres magros."
+  },
+  {
+    imagem: "../images/bifes/bife patinho.png",
+    descricao: "Bife de Patinho - Corte leve, ideal para dietas."
+  },
+  {
+    imagem: "../images/bifes/picanha inteira.png",
+    descricao: "Picanha - Corte nobre, indispensável no churrasco."
+  },
+  {
+    imagem: "../images/bifes/bife picanha.png",
+    descricao: "Bife de Picanha - Saboroso e com gordura na medida certa."
+  },
+  {
+    imagem: "../images/bifes/file mignon.png",
+    descricao: "Filé Mignon - Corte mais macio, excelente para grelhar."
+  },
+  {
+    imagem: "../images/bifes/bife file mignon.png",
+    descricao: "Bife de Filé Mignon - Ideal para preparo rápido e delicado."
+  },
+  {
+    imagem: "../images/bifes/medalhao file mignon.png",
+    descricao: "Medalhão de Filé Mignon - Corte nobre, geralmente envolto em bacon."
+  },
+  {
+    imagem: "../images/bifes/strogonoff mignon.png",
+    descricao: "Filé Mignon em Tiras - Muito usado em strogonoff."
+  },
+  {
+    imagem: "../images/bifes/fraldinha.png",
+    descricao: "Fraldinha - Corte macio e saboroso, ótimo para grelha."
+  },
+  {
+    imagem: "../images/bifes/musculo.png",
+    descricao: "Músculo - Ideal para caldos, cozidos e sopas, com muito colágeno."
+  },
+  {
+    imagem: "../images/bifes/musculo moido.png",
+    descricao: "Músculo Moído - Excelente para molhos e preparos de longa cocção."
+  },
+  {
+    imagem: "../images/bifes/cupim pedaco.png",
+    descricao: "Cupim - Corte com bastante gordura, ideal para assar lentamente."
+  },
+  {
+    imagem: "../images/bifes/peito pedaco.png",
+    descricao: "Peito - Corte de cocção longa, perfeito para caldos e cozidos."
+  }
+];
+
+let index = 0;
+
+function atualizarSlide() {
+  document.getElementById("imagem").src = slides[index].imagem;
+  document.getElementById("descricao").textContent = slides[index].descricao;
+
+  document.getElementById("btnAnterior").disabled = index === 0;
+  document.getElementById("btnProximo").disabled = index === slides.length - 1;
+}
+
+function proximo() {
+  if (index < slides.length - 1) {
+    index++;
+    atualizarSlide();
+  }
+}
+
+function voltar() {
+  if (index > 0) {
+    index--;
+    atualizarSlide();
+  }
+}
+
+window.onload = atualizarSlide;
